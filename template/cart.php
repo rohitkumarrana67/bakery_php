1e4fff4d81f93093e32f9e7d823d3dbef18a71a9
yatharth <yatharthverma070@gmail.com>
2020-03-27 15:25:07 +0530

    ADD remove action

--- template/cart.php
+++ template/cart.php
@@ -2,6 +2,15 @@
 	session_start();
 	include("../php/sqldb.php");
 
+	if(isset($_GET['action'])){
+		if($_GET['action'] == "delete"){
+			$sql = "DELETE FROM " . $_SESSION['username'] . "_cart where id =" . $_GET['id'];
+
+			$result = mysqli_query($conn,$sql);
+			header("Location: cart.php");						
+		}
+	}
+
 	$sql = "SELECT * FROM " . $_SESSION['username'] . "_cart";
 
 	$result = mysqli_query($conn,$sql);
@@ -26,23 +35,26 @@
 		      <th scope="col">Price</th>
 		      <th scope="col">Quantity</th>
 		      <th scope="col">Total Price</th>
+		      <th scope="col">Action</th>
 		    </tr>
 		  </thead>
 		  <tbody>
+		  	<?php $i=1; ?>
 		    <?php foreach($list as $item): ?>
 		    <tr>
-		    	<th scope="row"><?php echo $item['id'] ?></th>
+		    	<th scope="row"><?php echo $i; $i+=1; ?></th>
 		      	<td><?php echo $item['name'] ?></td>
 			  	<td><?php echo $item['cost'] ?></td>
 				<td><?php echo $item['quantity'] ?></td>
 				<td><?php echo number_format($item['quantity']*$item['cost'])."Rs" ?></td>
+				<td><a href="cart.php?action=delete&id=<?php echo $item['id']?>"><span>Remove</span></a></td>
 		    </tr>
 		    <?php 
 		    	$total = $total + ($item['quantity']*$item['cost']);
 		    ?>
 		    <?php endforeach; ?>
 		    <tr>
-		    	<td colspan="4" align="right">Total</td>
+		    	<td colspan="5" align="right">Total</td>
 		    	<td><?php echo number_format($total,2)."Rs"; ?></td>
 		    </tr>
 		  </tbody>
